7a13f66873ffec122829d539e58c3cdbec6a721e
DaviMelo <dvdmelo2@yahoo.com.br>
2023-02-10 20:29:35 -0300

    exerc100223

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -26,21 +26,59 @@ public class Operadores {
     //System.out.println("Inverteu " + !verdadeiro);
 
     // classe Operadores.java
-    int a, b;
+    //int a, b;
 
-    a = 6;
-    b = 6;
+    //a = 6;
+    //b = 5;
 
-    String resultado = a == b ? "verdadeiro" : "falso";
+    
+
+   // String resultado = a==b ?"verdadeiro":"falso";
 
     // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
     //if(a==b)
-        //resultado = " ";
+       //resultado = "verdadeiro";
     //else
         //resultado = "falso";
 
-    System.out.println(resultado);
+    //System.out.println(resultado);
     
+    //classe Operadores.java
+    int numero1 = 1;
+    int numero2 = 2;
+
+    if(numero1 > numero2)
+	    System.out.print("Numero 1 maior que numero 2");
+
+    if(numero1 < numero2)
+	    System.out.print("Numero 1 menor que numero 2");
+
+    if(numero1 >= numero2)
+	    System.out.print("Numero 1 maior ou igual que numero 2");
+
+    if(numero1 <= numero2)
+	    System.out.print("Numero 1 menor ou igual que numero 2");
+
+    if(numero1 != numero2)
+	    System.out.print("Numero 1 é diferente de numero 2");
+    
+        if(condicao1 && condicao2)
+        System.out.print("Os dois valores precisam ser verdadeiros");;
+    
+    //Se condicao1 OU condicao2 for verdadeira, executar código.
+
+    // Operadores.java
+boolean condicao1=true;
+
+boolean condicao2=false;
+
+/* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+expressões. 
+É como se estivesse escrito:
+ "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+*/
+    if(condicao1 || condicao2)
+        System.out.print("Um dos valores precisa ser verdadeiro");
 
     
     	
